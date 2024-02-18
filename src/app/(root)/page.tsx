2797699d09292c615dcfcf605e581da7c533c30e
Chermann-KING <codes.smiling@gmail.com>
2024-02-18 13:26:50 +0100

    💄 DEVELOP: Components > Layout > Add <GetInTouch />

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Features from "@/components/layout/Features";
+import GetInTouch from "@/components/layout/GetInTouch";
 import Hero from "@/components/layout/Hero";
 import Services from "@/components/layout/Services";
 import styled from "styled-components";
@@ -11,6 +12,7 @@ export default function Home() {
       <Hero />
       <Services />
       <Features />
+      <GetInTouch />
     </MainStyled>
   );
 }
@@ -26,7 +28,6 @@ const MainStyled = styled.main`
 
   margin: 0 auto;
   margin-top: 67px;
-  margin-bottom: 60px;
 
   // Styles pour les tablettes
   @media (max-width: 768px) {
---
+++ src/components/layout/GetInTouch.tsx
@@ -0,0 +1,122 @@
+import styled from "styled-components";
+
+const GetInTouch = () => {
+  return (
+    <SectionStyled>
+      <ContentStyled>
+        <h2>Let’s talk about your project</h2>
+        <p>
+          Ready to take it to the next level? Contact us today and find out how
+          our expertise can help your business grow.
+        </p>
+      </ContentStyled>
+      <button>Get In Touch</button>
+    </SectionStyled>
+  );
+};
+
+const SectionStyled = styled.section`
+  margin-bottom: -70px;
+
+  background-color: #e7816b;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  height: 292px;
+
+  background-image: url("/images/shared/desktop/bg-pattern-call-to-action.svg");
+  background-position: 823px 450px;
+
+  padding: 0 94px;
+  border-radius: 15px;
+
+  button {
+    cursor: pointer;
+    border: none;
+    min-width: 152px;
+    padding: 20px;
+    background-color: #fff;
+    color: #333136;
+    text-transform: uppercase;
+    font-weight: 600;
+    border-radius: 8px;
+
+    &:hover {
+      color: #fff;
+      background-color: #ffad9b;
+    }
+  }
+
+  // Styles pour les tablettes
+  @media (max-width: 768px) {
+    margin-bottom: -86px;
+
+    flex-direction: column;
+    justify-content: center;
+    gap: 32px;
+  }
+
+  // Styles pour les téléphones
+  @media (max-width: 480px) {
+    margin-bottom: -190px;
+
+    height: 379px;
+    padding: 0 25px;
+    background-position: 653px 480px;
+  }
+`;
+
+const ContentStyled = styled.div`
+  margin-right: 20px;
+
+  width: 459px;
+  height: 148px;
+  color: white;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  h2 {
+    font-size: 2.5rem;
+    font-weight: 500;
+    letter-spacing: 0;
+    line-height: 40px;
+  }
+
+  p {
+    line-height: 26px;
+  }
+
+  // Styles pour les tablettes
+  @media (max-width: 768px) {
+    margin-right: 0px;
+
+    text-align: center;
+    align-items: center;
+
+    h2 {
+      width: 335px;
+    }
+    p {
+      padding: 0 10px;
+    }
+  }
+
+  // Styles pour les téléphones
+  @media (max-width: 480px) {
+    width: 100%;
+    height: 163px;
+    h2 {
+      font-size: 2rem;
+      line-height: 36px;
+      width: 279px;
+    }
+    p {
+      padding: 0;
+    }
+  }
+`;
+
+export default GetInTouch;
