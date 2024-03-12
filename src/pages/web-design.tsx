0b663304898098b40a3369c97ff0750c2971db9a
Chermann-KING <codes.smiling@gmail.com>
2024-03-12 16:49:20 +0100

    🗃️ Develop: Web-Design > Add articles data

--- src/pages/web-design.tsx
+++ src/pages/web-design.tsx
@@ -6,6 +6,51 @@ import Image from "next/image";
 import React from "react";
 import styled from "styled-components";
 
+//Données pour les articles
+const articles = [
+  {
+    imageAlt: "Photo article Express",
+    imageSrc: "/images/web-design/desktop/image-express.jpg",
+    title: "EXPRESS",
+    description: "A multi-carrier shipping website for ecommerce businesses",
+  },
+  {
+    imageAlt: "Photo article Transfer",
+    imageSrc: "/images/web-design/desktop/image-transfer.jpg",
+    title: "TRANSFER",
+    description:
+      "Site for low-cost money transfers and sending money within seconds",
+  },
+  {
+    imageAlt: "Photo article Photon",
+    imageSrc: "/images/web-design/desktop/image-photon.jpg",
+    title: "PHOTON",
+    description:
+      "A state-of-the-art music player with high-resolution audio and DSP effects",
+  },
+  {
+    imageAlt: "Photo article Builder",
+    imageSrc: "/images/web-design/desktop/image-builder.jpg",
+    title: "BUILDER",
+    description:
+      "Connects users with local contractors based on their location",
+  },
+  {
+    imageAlt: "Photo article Blogr",
+    imageSrc: "/images/web-design/desktop/image-blogr.jpg",
+    title: "BLOGR",
+    description:
+      "Blogr is a platform for creating an online blog or publication",
+  },
+  {
+    imageAlt: "Photo article Camp",
+    imageSrc: "/images/web-design/desktop/image-camp.jpg",
+    title: "CAMP",
+    description:
+      "Get expert training in coding, data, design, and digital marketing",
+  },
+];
+
 export default function WebDesign() {
   return (
     <>
